8e431537b80b741df818d2870f9d4282ae8174bf
bit <50826868+Benzoleum@users.noreply.github.com>
2025-05-27 21:10:16 +0100

    implementing DB connection

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,26 +1,24 @@
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.meta.api.methods.CopyMessage;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.yaml.snakeyaml.Yaml;
 
 import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.Map;
 
 public class Bot extends TelegramLongPollingBot {
 
     private String botToken;
     private String botUsername;
-    protected Long selfId;
-    private boolean screaming = false;
-
+    Connection conn;
+    HashMap<Long, Integer> userMessages = new HashMap<>();
 
     public Bot() {
         loadConfig();
     }
-
 
     private void loadConfig() {
         Yaml yaml = new Yaml();
@@ -34,69 +32,61 @@ public class Bot extends TelegramLongPollingBot {
         }
     }
 
-
     @Override
     public void onUpdateReceived(Update update) {
+        registerIncrementMessages(update);
+    }
+
+    public void registerIncrementMessages(Update update) {
         var msg = update.getMessage();
         var user = msg.getFrom();
-        selfId = user.getId();
-        System.out.println("Registering user: " + user.getId());
-        System.out.println(user.getFirstName() + " wrote " + msg.getText());
-        System.out.println("Sending message to user: " + user.getId());
-        if (msg.isCommand()) {
-            if (msg.getText().equals("/scream"))         //If the command was /scream, we switch gears
-                screaming = true;
-            else if (msg.getText().equals("/whisper"))  //Otherwise, we return to normal
-                screaming = false;
-
-            return;                                     //We don't want to echo commands, so we exit
+        Long userId = user.getId();
+        if (userMessages.containsKey(userId)) {
+            userMessages.put(userId, userMessages.get(userId) + 1);
         }
-        if (screaming)                            //If we are screaming
-            scream(selfId, update.getMessage());     //Call a custom method
-        else
-            copyMessage(selfId, msg.getMessageId()); //Else proceed normally
-    }
-
-    private void scream(Long id, Message msg) {
-        if (msg.hasText())
-            sendText(id, msg.getText().toUpperCase());
-        else
-            copyMessage(id, msg.getMessageId());  //We can't really scream a sticker
-    }
-
-    public void sendText(Long who, String what) {
-        SendMessage sm = SendMessage.builder()
-                .chatId(who.toString()) //Who are we sending a message to
-                .text(what).build();    //Message content
-        try {
-            execute(sm);                        //Actually sending the message
-        } catch (TelegramApiException e) {
-            throw new RuntimeException(e);      //Any error will be printed here
+        if (!userMessages.containsKey(userId)) {
+            userMessages.put(userId, 1);
+            System.out.println("New user registered: " + user.getId());
+            insertNewUser(userId, user.getUserName());
         }
-    }
-
-    public void copyMessage(Long who, Integer msgId) {
-        CopyMessage cm = CopyMessage.builder()
-                .fromChatId(who.toString())  //We copy from the user
-                .chatId(who.toString())      //And send it back to him
-                .messageId(msgId)            //Specifying what message
-                .build();
-        try {
-            execute(cm);
-        } catch (TelegramApiException e) {
-            throw new RuntimeException(e);
-        }
+        System.out.println("User: " + userId + ", messages: " + userMessages.get(userId));
     }
 
     @Override
     public String getBotToken() {
         return botToken;
-//        return "7373436976:AAEEbIWqvcvrRuQpHQl2vzbJkoynwKkyrb8";
     }
 
     @Override
     public String getBotUsername() {
         return botUsername;
-//        return "weekly_message_counter_bot";
     }
+
+    public void initialiseDbConnection() {
+        String url = "jdbc:sqlite:/Users/benzoleum/Desktop/Personal_2/WeeklyMessageCounterTgBot/src/db/users.db"; // Path to SQLite database
+        try {
+            conn = DriverManager.getConnection(url);
+            if (conn != null) {
+                System.out.println("DB connection established");
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void insertNewUser(Long userId, String username) {
+        try {
+            String sql = "INSERT INTO users (user_id, username, message_count, first_message) VALUES (?, ?, ?, ?)";
+            var pstmt = conn.prepareStatement(sql);
+            pstmt.setLong(1, userId);
+            pstmt.setString(2, username);
+            pstmt.setInt(3, 1);
+            pstmt.setLong(4, System.currentTimeMillis());
+            pstmt.executeUpdate();
+            System.out.println("Inserted new user: " + userId);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,10 +4,9 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 public class Main {
     public static void main(String[] args) throws TelegramApiException {
-        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
         Bot bot = new Bot();
+        bot.initialiseDbConnection();
+        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
         botsApi.registerBot(bot);
-//        bot.sendText(bot.selfId, "Hello World!");
-        bot.sendText(59612513L, "Hello World!");
     }
 }
